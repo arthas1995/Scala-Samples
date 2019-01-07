c35d5ce8d3039fd9e2c2b675e38f1ad78f59fc0d
arthas <jlj_vip98@qq.com>
2019-01-07 11:16:02 +0800

    fix

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,14 @@
+/**
+  * @author: Arthas
+  * @date: 2019-01-07 11:01
+  * @description:
+  */
+class HelloWorld {
+
+
+  def main(args: Array[String]): Unit = {
+    val a = 1;
+    printf("Scala" + a);
+  }
+
+}
