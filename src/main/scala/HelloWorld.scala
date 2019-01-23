7bb61a9d1b47f6760c61b60aec23ff7495ac180b
arthas <jlj_vip98@qq.com>
2019-01-23 15:32:18 +0800

    scala hello

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,10 +1,9 @@
 /**
   * @author: Arthas
   * @date: 2019-01-07 11:01
-  * @description:
+  * @description:  var 可以被多次赋值，val 不能再被赋值
   */
 class HelloWorld {
-
 
   def main(args: Array[String]): Unit = {
     val a = 1;
---
+++ src/main/scala/learn/TopStock.scala
@@ -0,0 +1,12 @@
+package learn
+
+/**
+  * @author: Arthas
+  * @date: 2019-01-23 15:21
+  * @description:
+  */
+class TopStock {
+
+
+
+}
